e98bda7b111900a28ea4d0fe4367151dc1e6b8a6
Anna Plachkova <annaplachkova@yahoo.co.uk>
2026-07-09 14:04:23 +0300

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -1,1 +1,15 @@
+#!/bin/bash
+# Simple Interest Calculator
 
+echo "Enter principal amount:"
+read principal
+
+echo "Enter rate of interest (%):"
+read rate
+
+echo "Enter time period (years):"
+read time
+
+simple_interest=$(echo "scale=2; $principal * $rate * $time / 100" | bc)
+
+echo "Simple Interest: $simple_interest"
